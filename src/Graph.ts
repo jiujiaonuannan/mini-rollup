dcb11d806921739ea370bae3419bf0fba89761da
Madman804 <alvin.wang1202@gmail.com>
2022-06-16 23:06:42 +0200

    chore: init project

---
+++ src/Graph.ts
@@ -0,0 +1,33 @@
+// 模块依赖图对象的实现
+import { dirname, resolve } from 'path';
+export class Graph {
+  entryPath: string;
+  basedir: string;
+  moduleById: Record<string, Module> = {};
+  modules: Module[] = [];
+
+  constructor(options: GraphOptions) {
+    const { entry, bundle } = options;
+    this.entryPath = resolve(entry);
+    this.basedir = dirname(this.entryPath);
+    this.bundle = bundle;
+  }
+  
+  async build() {
+    // 1. 获取并解析模块信息
+    // 2. 构建依赖关系图
+    // 3. 模块拓扑排序
+    // 4. Tree Shaking, 标记需要包含的语句
+  }
+  
+  getModuleById(id: string) {
+    return this.moduleById[id];
+  }
+
+  addModule(module: Module) {
+    if (!this.moduleById[module.id]) {
+      this.moduleById[module.id] = module;
+      this.modules.push(module);
+    }
+  }
+}
